5d7729c3d468380af6c8571f7603e8fd2394e41a
mesto <mhmdmestrah53@gmail.com>
2023-04-01 07:15:20 +0300

    first comit

---
+++ components/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:mhmdmestrah@gmail.com">mhmdmestrah@gmail.com</a>
+         <a href="mailto:hadirahal@gmail.com">hadirahal@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>07:00am to 01:00am</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">lebanon ,biurut</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="76680732">76680732</a>
+         <a href="tel:81657520">81675520</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Mohammad mestrah, hadi rahal</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
